0d5dafe8fc0ea7eb30dba4a9b3be23e11ef951ea
embold-tyler <tyler@embold.com>
2024-03-12 13:28:05 -0700

    Shouldn't need to rehash here

--- coder/scripts/ruby-install.sh
+++ coder/scripts/ruby-install.sh
@@ -4,5 +4,4 @@ RUBY_VERSION=${RUBY_VERSION:-3.0.6}
 
 rbenv install --skip-existing $RUBY_VERSION \
     && rbenv global $RUBY_VERSION \
-    && rbenv rehash \
     && gem install bundler colorls /coder/pulsar.gem --conservative
